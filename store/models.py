d2f80c4c6f0493104b56a624acd9906ddd7ca390
Tanmoy-Sarkar <tanmoy9920@gmail.com>
2021-07-31 14:21:21 +0600

    added some property decorators

--- store/models.py
+++ store/models.py
@@ -31,9 +31,31 @@ class Order(models.Model):
     customer = models.ForeignKey(Customer,on_delete=models.SET_NULL,blank=True,null=True)
     date_ordered = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False,null=True,blank=True)
+    transaction_id = models.CharField(max_length=200,null=True)
 
     def __str__(self):
-        return self.id
+        return str(self.id)
+
+    @property
+    def shipping(self):
+        shipping = False
+        orderitems = self.orderitem_set.all()
+
+        for i in orderitems:
+            if i.product.digital == False:
+                shipping = True
+
+        return shipping
+
+    @property
+    def get_cart_total(self):
+        items = self.orderitem_set.all()
+        total = sum([ item.get_total for item in items])
+        return total
+    def get_cart_items(self):
+        items = self.orderitem_set.all()
+        total = sum([ item.quantity for item in items])
+        return total
 
 class OrderItem(models.Model):
     product = models.ForeignKey(Product,on_delete=models.SET_NULL,blank=True,null=True)
@@ -43,6 +65,11 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return self.product.name
+
+    @property
+    def get_total(self):
+        total = self.product.price*self.quantity
+        return total
 
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer,on_delete=models.SET_NULL,blank=True,null=True)
